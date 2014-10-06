3853c040753c81ab813172d74479708c45a7a683
Dan Buch <dan@meatballhat.com>
2014-10-06 08:22:36 -0400

    TRIVIAL replacing some double quotes with single quotes

--- tests/docker_ps_images2_map.py
+++ tests/docker_ps_images2_map.py
@@ -1,6 +1,6 @@
 containers = dict()
-containers['6b76b45873ae'] = "c260c5345f656b271a2d587641d1bda95dc7599a83016d7249ff241ca4a8a8c8"  # noqa
-containers['c8b7beeebbf6'] = "6b4e8a7373fe8706183f15dc367564a723710f2214cab23d14d195d8abd8eccb"  # noqa
-containers['6b44562d0ff6'] = "6b4e8a7373fe8706183f15dc367564a723710f2214cab23d14d195d8abd8eccb"  # noqa
-containers['f71977673be2'] = "75af0ca1d93cad44e4d00ffa7d6c74862dc7f29bf1afda75f80edfc47a65633e"  # noqa
-containers['e9082a00a862'] = "c260c5345f656b271a2d587641d1bda95dc7599a83016d7249ff241ca4a8a8c8"  # noqa
+containers['6b76b45873ae'] = 'c260c5345f656b271a2d587641d1bda95dc7599a83016d7249ff241ca4a8a8c8'  # noqa
+containers['c8b7beeebbf6'] = '6b4e8a7373fe8706183f15dc367564a723710f2214cab23d14d195d8abd8eccb'  # noqa
+containers['6b44562d0ff6'] = '6b4e8a7373fe8706183f15dc367564a723710f2214cab23d14d195d8abd8eccb'  # noqa
+containers['f71977673be2'] = '75af0ca1d93cad44e4d00ffa7d6c74862dc7f29bf1afda75f80edfc47a65633e'  # noqa
+containers['e9082a00a862'] = 'c260c5345f656b271a2d587641d1bda95dc7599a83016d7249ff241ca4a8a8c8'  # noqa
